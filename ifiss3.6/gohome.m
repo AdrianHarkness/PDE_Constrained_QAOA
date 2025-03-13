c311b663b196bff1ae3bc389cd63803e1fd4a6ea
adrianharkness <adh323@lehigh.edu>
2025-03-13 15:55:00 -0400

    first commit

---
+++ ifiss3.6/gohome.m
@@ -0,0 +1,7 @@
+%GOHOME positions command prompt at top level directory
+%   IFISS scriptfile: DJS; 9 January 2019.
+% Copyright (c) 2005 D.J. Silvester, H.C. Elman, A. Ramage (see readme.m)
+%cd('/home/caas63/ifiss3.6');
+%cd('/fs/helios/elman/ifiss/ifiss3.6');
+%cd('c:\Documents and Settings\elman\My Documents\MATLAB\ifiss3.6');
+cd('/Users/adrianharkness/QCOL_COPT/QAOA/PDE_Constrained_QAOA/ifiss3.6/')
